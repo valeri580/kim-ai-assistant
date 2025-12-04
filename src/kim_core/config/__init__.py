af7ed9ea23a1bdcf3796ca68d1476871ad5a0074
Валерий <valeriperehodov@gmail.com>
2025-12-04 11:36:08 +0300

    feat: добавлена конфигурация и логирование в проект, обновлена документация

--- src/kim_core/config/__init__.py
+++ src/kim_core/config/__init__.py
@@ -1,2 +1,6 @@
 """Конфигурация проекта."""
 
+from kim_core.config.settings import AppConfig, load_config
+
+__all__ = ["AppConfig", "load_config"]
+
--- src/kim_core/logging/__init__.py
+++ src/kim_core/logging/__init__.py
@@ -1,2 +1,8 @@
 """Логирование и счётчики токенов."""
 
+from loguru import logger
+
+from kim_core.logging.logger import init_logger
+
+__all__ = ["logger", "init_logger"]
+
